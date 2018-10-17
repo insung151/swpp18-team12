325bc09aa0dcb0892f25c275d0012bd3e0105a17
Inseong Hwang <insung151@naver.com>
2018-10-18 02:29:49 +0900

    Add validation for rangefield

--- backend/snuclub/core/fields.py
+++ backend/snuclub/core/fields.py
@@ -1,9 +1,16 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 class IntegerRangeField(models.IntegerField):
-    def __init__(self, min_value=None, max_value=None, *args, **kwargs):
-        super(IntegerRangeField, self).__init__(*args, **kwargs)
+    def __init__(self, min_value=None, max_value=None, validators=[], *args, **kwargs):
+        self.min_value = min_value
+        self.max_value = max_value
+        validators += [
+            MinValueValidator(min_value),
+            MaxValueValidator(max_value)
+        ]
+        super(IntegerRangeField, self).__init__(validators=validators, *args, **kwargs)
 
     def formfield(self, **kwargs):
         defaults = {'min_value': self.min_value, 'max_value': self.max_value}
