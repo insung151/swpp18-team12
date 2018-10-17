efb4549c1ff682d3bce8d0f54f5f30d73e796f67
Inseong Hwang <insung151@naver.com>
2018-10-17 19:35:41 +0900

    Create custom user model and user profile model

---
+++ backend/snuclub/core/fields.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, min_value=None, max_value=None, *args, **kwargs):
+        super(IntegerRangeField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value': self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
