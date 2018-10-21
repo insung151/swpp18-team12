cafe63a2d0c8893b537b4b6998ea36adcfdc7e1d
HoJoon Song <hojunroks@snu.ac.kr>
2018-10-21 20:21:54 +0900

    Add url configuration for email verification

--- backend/snuclub/accounts/urls.py
+++ backend/snuclub/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from accounts.views import LoginApiView, LogoutApiView, SignUpApiView, ChangePasswordAPIView
+from accounts.views import LoginApiView, LogoutApiView, SignUpApiView, ChangePasswordAPIView,ActivateUserAPIView
 
 urlpatterns = [
     path('login/', LoginApiView.as_view(), name='login'),
     path('logout/', LogoutApiView.as_view(), name='logout'),
     path('signup/', SignUpApiView.as_view(), name='signup'),
     path('change_password/', ChangePasswordAPIView.as_view(), name='change_password'),
+    path(r'activate/<userid>/<token>/',ActivateUserAPIView.as_view(), name='activate'),
 ]
