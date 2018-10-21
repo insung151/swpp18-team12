44d30c76335b6262023a8b7c810bd4ccbb553242
snumath <yj8902@naver.com>
2018-10-21 16:35:06 +0900

    Fix compiler error. solve #5

--- frontend/src/app/service/authentication.service.ts
+++ frontend/src/app/service/authentication.service.ts
@@ -12,7 +12,7 @@ export class AuthenticationService {
   // TODO: connect backend and frontend (solve CRSF problem)
   async logIn(email: string, password: string): Promise<boolean> {
     const url = 'api/account/login';
-    const res = await this.http.post(url,
+    const res: any = await this.http.post(url,
       {'email': email, 'password': password }, { headers: getCSRFHeaders(), withCredentials: true})
       .toPromise();
     try {
@@ -50,49 +50,60 @@ export class AuthenticationService {
   // TODO: Error handling
   async signUp(
     email: string,
-    username: string,
     password: string,
     password_confirmation: string,
-    admission: string,
-    department): Promise<boolean> {
+    username: string,
+    admission: number,
+    department: string): Promise<boolean> {
     const url = 'api/account/signup';
-    const res = await this.http.post(url,
-      {email: email, username: username, password: password, admission: admission, department: department},
+    const res: any = await this.http.post(url,
+      { email: email,
+        password: password,
+        password_confirmation: password_confirmation,
+        username: username,
+        admission: admission,
+        department: department},
       { headers: getCSRFHeaders() })
       .toPromise();
     try {
-      if (this.status === 201) {
-
+      if (res.status === 201) {
+        return true;
+      } else if (res.status === 400) {
+        alert('Bad Request'); // TODO: Handle bad request
       }
-    } catch {
-
+    } catch (e) {
+      console.dir(e); // TODO: Make Error Handler
     }
   }
 
   /*
   TODO: Finish changePassword. 1) check valid user 2) check pw change 3) put api
-  async changePassword(password: string, password_confirmation: string): Promise<boolean> {
+  */
+
+  
+  async changePassword(old_password: string, new_password: string): Promise<boolean> {
     const url = 'api/account/change_password';
-    const res = await this.http.post(url, {'password': password }, { headers: getCSRFHeaders(), withCredentials: true})
+    const res: any = await this.http.put(url,
+      {'old_password': old_password, 'new_password': new_password },
+      { headers: getCSRFHeaders(), withCredentials: true})
       .toPromise();
     try {
-      // Successful login
+      // Successfully changed
       if (res.status === 200) {
-        let token = '';
-        if (document.cookie) {
-          token = document.cookie.split('csrftoken=')[ 1 ].split(';')[ 0 ];
-        }
-        localStorage.setItem('currentUser', JSON.stringify({ 'token' : token }));
+        // let token = '';
+        // if (document.cookie) {
+        //   token = document.cookie.split('csrftoken=')[ 1 ].split(';')[ 0 ];
+        // }
+        // localStorage.setItem('currentUser', JSON.stringify({ 'token' : token }));
         return true;
       } else if (res.status === 401) {
-        // 'Email verification is incomplete or your account information is incorrect.'
         alert(res.message);
       }
     } catch {
       // TODO: Error Handler
     }
   }
-  */
+  
 
   constructor(private http: HttpClient) { }
 }
