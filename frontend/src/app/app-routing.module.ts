e4f5e98ea289dcd5db3d8c95215aa590971c0b71
snumath <yj8902@naver.com>
2018-10-25 08:07:20 +0900

    Create skeleton for reset password

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { MainComponent } from './main/main.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'sign_in', component: SignInComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'reset_password', component: ResetPasswordComponent },
+  { path: 'change_password', component: ChangePasswordComponent },
 ];
 
 @NgModule({
