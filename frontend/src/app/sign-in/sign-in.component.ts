b3e975dc02c0caff4e2b79f53749a84f5f34ee2b
snumath <yj8902@naver.com>
2018-10-25 07:54:23 +0900

    Change signin coding style to signup

--- frontend/src/app/sign-in/sign-in.component.ts
+++ frontend/src/app/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-// import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthenticationService } from '../service/authentication.service';
@@ -12,23 +12,31 @@ import { AuthenticationService } from '../service/authentication.service';
 })
 export class SignInComponent implements OnInit {
 
+  private signInForm: FormGroup;
   private previousUrl: string;
+
+  private email: AbstractControl;
+  private password: AbstractControl;
 
 
   constructor(private authenticationService: AuthenticationService,
-              private router: Router) { }
+              private router: Router,
+              private formBuilder: FormBuilder) {
+    this.signInForm = formBuilder.group({
+      'email': ['', ],
+      'password': ['', ],
+    });
+    this.email = this.signInForm.controls['email'];
+    this.password = this.signInForm.controls['password'];
+  }
 
-  async logIn(email: string, password: string): Promise<void> {
-    const res: boolean = await this.authenticationService.logIn(email, password);
+  async logIn(): Promise<void> {
+    const res: boolean = await this.authenticationService.logIn(this.email.value, this.password.value);
     if (res) {
       this.router.navigateByUrl(this.previousUrl); // login success
     } else {
       alert('login failed'); // TODO: login failed
     }
-  }
-
-  click(): void {
-    this.router.navigateByUrl('/signup');
   }
 
   ngOnInit() {
