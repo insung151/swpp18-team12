bd904138967ddc17befd04c53ab8e799bfe2f173
snumath <yj8902@naver.com>
2018-10-25 07:56:53 +0900

    Create reset password component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'sign_in', component: SignInComponent },
   { path: 'signup', component: SignUpComponent },
+  { path: 'reset_password', component: ResetPasswordComponent },
 ];
 
 @NgModule({
