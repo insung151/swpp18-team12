19755d42c90b9f12bc574e19f8083f4247cf6d42
snumath <yj8902@naver.com>
2018-10-25 13:02:09 +0900

    Split authentification works into header

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-
-import { AuthenticationService } from '../service/authentication.service';
-
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -12,19 +7,9 @@ import { AuthenticationService } from '../service/authentication.service';
 })
 export class MainComponent implements OnInit {
 
-  private loggedIn: boolean;
-
-  constructor(private authenticationService: AuthenticationService,
-              private router: Router) { }
+  constructor() { }
 
   ngOnInit() {
-    this.loggedIn = this.authenticationService.loggedIn();
-  }
-
-  logout(): void {
-    if (this.authenticationService.logOut()) {
-      this.loggedIn = false;
-    }
   }
 
 }
