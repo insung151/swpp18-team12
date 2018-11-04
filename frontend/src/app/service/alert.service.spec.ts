935c5457e15504ee51b0c4b0c2861581b0722cf5
snumath <yj8902@naver.com>
2018-11-04 21:28:10 +0900

    Add test code for account_front alert.service

--- frontend/src/app/service/alert.service.spec.ts
+++ frontend/src/app/service/alert.service.spec.ts
@@ -1,11 +1,29 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, Subject } from 'rxjs';
 
 import { AlertService } from './alert.service';
+import { Component } from '@angular/core';
+
+@Component({
+  template: ``
+})
+class MockComponent { }
 
 describe('AlertService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AlertService]
+      declarations: [
+        MockComponent,
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([{
+          path: '**', component: MockComponent,
+        }]),
+      ],
+      providers: [
+        AlertService,
+      ]
     });
   });
 
