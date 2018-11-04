0ced8f0d9480905c5347d20101642230dac2124e
snumath <yj8902@naver.com>
2018-11-04 22:08:11 +0900

    Add test code for reset_password

--- frontend/src/app/reset-password/reset-password.component.spec.ts
+++ frontend/src/app/reset-password/reset-password.component.spec.ts
@@ -6,6 +6,8 @@ import { AuthenticationService } from '../service/authentication.service';
 import { AuthenticationServiceSpy } from '../service/authentication.service.spy';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AlertService } from '../service/alert.service';
+import { AlertServiceSpy } from '../service/alert.service.spy';
 
 describe('ResetPasswordComponent', () => {
   let component: ResetPasswordComponent;
@@ -22,7 +24,8 @@ describe('ResetPasswordComponent', () => {
         ReactiveFormsModule,
       ],
       providers: [
-        { provide: AuthenticationService, useClass: AuthenticationServiceSpy }
+        { provide: AuthenticationService, useClass: AuthenticationServiceSpy },
+        { provide: AlertService, useClass: AlertServiceSpy },
       ]
     })
     .compileComponents();
@@ -37,5 +40,15 @@ describe('ResetPasswordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Forgot password', () => {
+    it('get email', async(() => {
+      const email = component.email.value;
+      component.forgotPassword().then(() => {
+        fixture.detectChanges();
+        expect(authenticationServiceSpy.forgotPassword).toHaveBeenCalledWith(email);
+      });
+    }));
   });
 });
