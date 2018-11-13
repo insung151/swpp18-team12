794aaa38ab3217a66d2589d197176aeb86038e83
HoJoon Song <hojunroks@snu.ac.kr>
2018-11-13 14:27:46 +0900

    Debug tag update for serializer

--- backend/snuclub/club/serializers.py
+++ backend/snuclub/club/serializers.py
@@ -1,7 +1,7 @@
 import re
 import json
 from rest_framework import serializers
-from club.models import Club, ClubDetail, Tag
+from club.models import Club, ClubDetail, Tag, ClubRating
 from django.core.exceptions import ObjectDoesNotExist
 
 
@@ -24,12 +24,9 @@ class ClubSerializer(serializers.ModelSerializer):
                 ]
     tags = TagSerializer(many=True, default=[])
 
-    def validate_name(self, value):
-        if Club.objects.filter(name=value).exists():
-            raise serializers.ValidationError('Already exists')
-        return value
-
     def create(self, validated_data):
+        if Club.objects.filter(name=validated_data.get('name')).exists():
+            raise serializers.ValidationError('Already exists')
         tags_data = validated_data.pop('tags')
         club = Club(**validated_data)
         for tag in tags_data:
@@ -41,16 +38,32 @@ class ClubSerializer(serializers.ModelSerializer):
             club.save()
             club.tags.add(add_tag)
         club.save()
+        club_detail = ClubDetail(club=club)
+        club_detail.save()
+        club_rating = ClubRating(club=club)
+        club_rating.save()
         return club
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
+        if instance.name != validated_data.get('name'):
+            if Club.objects.filter(name=validated_data.get('name')).exists():
+                raise serializers.ValidationError('Already exists')
+            else:
+                instance.name = validated_data.get('name', instance.name)
         instance.profile_image = validated_data.get('profile_image', instance.profile_image)
         instance.short_description = validated_data.get('short_description', instance.short_description)
         instance.activity_type = validated_data.get('activity_type', instance.activity_type)
         instance.category = validated_data.get('category', instance.category)
         instance.subcategory = validated_data.get('subcategory', instance.subcategory)
-        instance.tags = validated_data.get('tags', instance.tags)
+        instance.tags.clear()
+        tags_data = validated_data.get('tags')
+        for tag in tags_data:
+            try:
+                add_tag = Tag.objects.get(name=tag.get('name'))
+            except ObjectDoesNotExist:
+                add_tag = Tag(**tag)
+                add_tag.save()
+            instance.tags.add(add_tag)
         instance.save()
         return instance
 
@@ -64,14 +77,7 @@ class ClubDetailSerializer(serializers.ModelSerializer):
                   'long_description',
                   'history']
 
-    def update(self, instance, validated_data):
-        instance.join_due_datetime = validated_data.get('join_due_datetime', instance.join_due_datetime)
-        instance.join_link = validated_data('join_link', instance.join_link)
-        instance.site_link = validated_data('site_link', instance.site_link)
-        instance.long_description = validated_data('long_description', instance.long_description)
-        instance.history = validated_data('history', instance.history)
-        instance.save()
-        return instance
+
 
 
 class ChangeProfileImageSerializer(serializers.Serializer):
