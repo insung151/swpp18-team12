805a82c884f71211c5938c1d511f5ddac14f3e09
HoJoon Song <hojunroks@snu.ac.kr>
2018-11-13 14:28:58 +0900

    Add tests for club API

--- backend/snuclub/club/test_api.py
+++ backend/snuclub/club/test_api.py
@@ -5,6 +5,7 @@ from accounts.models import User, UserProfile
 from club.models import Club
 from club.user_rating.models import UserRating
 from club.user_rating.serializers import UserRatingListSerializer
+from club.serializers import ClubSerializer, ClubDetailSerializer, ChangeProfileImageSerializer, ChangeAdminSerializer
 
 
 class MockRequest:
@@ -26,13 +27,16 @@ class ClubTestCase(APITestCase):
         self.user = User.objects.create_user(
             email='testemail@email.com',
             username='testuser',
-            password='testpwd',
+            password='qwer1234',
         )
+        self.user.is_active = True
         self.user_profile = UserProfile.objects.create(
             user=self.user
         )
+        self.user.save()
         self.club = Club.objects.create(
             admin=self.user_profile,
+            name="test",
             activity_type=2,
             category=3,
             subcategory=18
@@ -55,3 +59,71 @@ class ClubTestCase(APITestCase):
         )
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.data["results"], serializer.data)
+
+    def test_club_detail(self):
+        #login
+        login_data = {
+            "email": "testemail@email.com",
+            "password": "qwer1234"
+        }
+        resp = self.client.post('/api/accounts/login/', data=login_data)
+        self.assertEqual(resp.status_code, 200)
+
+        # create
+        club_data = {
+            "name": "testClub",
+            "profile_image": None,
+            "activity_type": 1,
+            "short_description": "a",
+            "category": 1,
+            "subcategory": 2,
+            "tags": [
+                {
+                    "name": "tag2"
+                },
+                {
+                    "name": "tag3"
+                }
+            ]
+        }
+        resp = self.client.post(f'/api/club/new/', data=club_data, format='json')
+        self.assertEqual(resp.status_code, 201)
+
+        # get short
+        resp = self.client.get(f'/api/club/2/club_short/')
+        self.assertEqual(resp.data['activity_type'], 1)
+
+        # update short
+        update_club_data = {
+            "name": "testClub",
+            "profile_image": None,
+            "activity_type": 1,
+            "short_description": "Edited description",
+            "category": 1,
+            "subcategory": 2,
+            "tags": [
+                {
+                    "name": "tag2"
+                },
+                {
+                    "name": "tag3"
+                }
+            ]
+        }
+        resp = self.client.put(f'/api/club/2/club_short/', data=update_club_data, format='json')
+        self.assertEqual(resp.status_code, 200)
+
+        # update detail
+        update_club_detail_data = {
+            "join_due_datetime": None,
+            "join_link": "https://www.qw.com",
+            "site_link": "https://www.afdc.com",
+            "long_description": "blahblahblah",
+            "history": "2001"
+        }
+        resp = self.client.put(f'/api/club/2/club_detail/', data=update_club_detail_data, format='json')
+        self.assertEqual(resp.status_code, 200)
+
+        # get detail
+        resp = self.client.get(f'/api/club/2/club_detail/')
+        self.assertEqual(resp.data['history'], "2001")
