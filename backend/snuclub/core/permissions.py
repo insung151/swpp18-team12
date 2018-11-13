9f77d3e46e59992502a0cc7fcb2a79c38711dc6d
HoJoon Song <hojunroks@snu.ac.kr>
2018-11-13 14:28:50 +0900

    Add new permission types for club

--- backend/snuclub/core/permissions.py
+++ backend/snuclub/core/permissions.py
@@ -8,3 +8,17 @@ class IsUserRatingOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.user.user == request.user
+
+
+class IsClubDetailAdminOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.club.admin.user == request.user
+
+
+class IsClubAdminOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.admin.user == request.user
